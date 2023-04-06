9b1843cab7e1ba3c0b68b0613cf86a673f952076
Chaban Serhii <sergeitchaban@gmail.com>
2023-04-06 16:37:03 +0300

    Update bookmarks.page.tsx

--- src/pages/bookmarks.page.tsx
+++ src/pages/bookmarks.page.tsx
@@ -1,58 +1,118 @@
 import { type NextPage, type GetServerSideProps } from "next";
-import { type Loadout, type User } from "@prisma/client";
-import { type DestinyInventoryItemDefinition } from "bungie-api-ts/destiny2";
 import { TypographyLarge } from "~/components/typography";
 import { getServerAuthSession } from "~/server/auth";
 import { trpsSSG } from "~/utils/ssg";
 import { LoadoutPreviewCard } from "~/components/loadouts/LoadoutPreviewCard";
 import { useAuthUser } from "~/hooks/useAuthUser";
+import { trpcNext } from "~/utils/api";
+import {
+  handleAuthUserLoadoutBookmark,
+  handleAuthUserLoadoutLike,
+} from "~/utils/loadout";
 
-interface BookmarksPageProps {
-  loadouts: Array<
-    Loadout & {
-      _count: { likes: number };
-      likes: {
-        likedByUserId: string;
-      }[];
-      bookmarks: {
-        savedByUserId: string;
-      }[];
-      author: User;
-    }
-  >;
-  inventoryItems: Record<string, DestinyInventoryItemDefinition>;
-}
+// interface BookmarksPageProps {
+//   loadouts: Array<
+//     Loadout & {
+//       _count: { likes: number };
+//       likes: {
+//         likedByUserId: string;
+//       }[];
+//       bookmarks: {
+//         savedByUserId: string;
+//       }[];
+//       author: User;
+//     }
+//   >;
+//   inventoryItems: Record<string, DestinyInventoryItemDefinition>;
+// }
 
-export const BookmarksPage: NextPage<BookmarksPageProps> = ({
-  loadouts,
-  inventoryItems,
-}) => {
+export const BookmarksPage: NextPage = () => {
   const [authUser] = useAuthUser();
+
+  const { data } = trpcNext.loadouts.getAuthBookmarked.useQuery();
+
+  const trpcCtx = trpcNext.useContext();
+
+  const likeMutation = trpcNext.loadouts.like.useMutation({
+    onMutate: async ({ loadoutId }) => {
+      await trpcCtx.loadouts.getAuthBookmarked.cancel();
+
+      const prevLoadouts = await trpcCtx.loadouts.getAuthBookmarked.getData();
+
+      trpcCtx.loadouts.getAuthBookmarked.setData(void 0, (old) => ({
+        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+        inventoryItems: old!.inventoryItems,
+        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+        loadouts: old!.loadouts.map((loadout) =>
+          handleAuthUserLoadoutLike({ loadout, loadoutId, authUser })
+        ),
+      }));
+
+      return { prevLoadouts };
+    },
+    onError: (_, __, ctx) =>
+      trpcCtx.loadouts.getAuthBookmarked.setData(
+        void 0,
+        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+        ctx!.prevLoadouts
+      ),
+    onSettled: () => trpcCtx.loadouts.getAuthBookmarked.invalidate(),
+  });
+
+  const saveMutation = trpcNext.loadouts.bookmark.useMutation({
+    onMutate: async ({ loadoutId }) => {
+      await trpcCtx.loadouts.getAuthBookmarked.cancel();
+
+      const prevLoadouts = await trpcCtx.loadouts.getAuthBookmarked.getData();
+
+      trpcCtx.loadouts.getAuthBookmarked.setData(void 9, (old) => ({
+        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+        inventoryItems: old!.inventoryItems,
+        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+        loadouts: old!.loadouts.map((loadout) =>
+          handleAuthUserLoadoutBookmark({ loadout, loadoutId, authUser })
+        ),
+      }));
+
+      return { prevLoadouts };
+    },
+    onError: (_, __, ctx) =>
+      trpcCtx.loadouts.getAuthBookmarked.setData(
+        void 0,
+        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+        ctx!.prevLoadouts
+      ),
+    onSettled: () => trpcCtx.loadouts.getAuthBookmarked.invalidate(),
+  });
+
+  const { loadouts, inventoryItems } = data || {};
+
+  const handleLikeLoadout = (loadoutId: string) =>
+    likeMutation.mutate({ loadoutId });
+
+  const handleSaveLoadout = (loadoutId: string) =>
+    saveMutation.mutate({ loadoutId });
 
   return (
     <div className="grid grid-cols-1 gap-2">
       <TypographyLarge>Saved loadouts</TypographyLarge>
-      {loadouts.map((loadout) => (
-        <LoadoutPreviewCard
-          key={loadout.id}
-          loadout={loadout}
-          inventoryItems={inventoryItems}
-          onLike={() => {
-            void 0;
-          }}
-          onSave={() => {
-            void 0;
-          }}
-          authUser={authUser}
-        />
-      ))}
+      {loadouts &&
+        inventoryItems &&
+        loadouts.map((loadout) => (
+          <LoadoutPreviewCard
+            key={loadout.id}
+            loadout={loadout}
+            inventoryItems={inventoryItems}
+            onLike={handleLikeLoadout}
+            onSave={handleSaveLoadout}
+            authUser={authUser}
+          />
+        ))}
     </div>
   );
 };
 
-export const getServerSideProps: GetServerSideProps<
-  BookmarksPageProps
-> = async (ctx) => {
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
   const session = await getServerAuthSession(ctx);
 
   if (!session)
@@ -65,10 +125,10 @@ export const getServerSideProps: GetServerSideProps<
 
   const trpc = trpsSSG(session);
 
-  const response = await trpc.loadouts.getAuthBookmarked.fetch();
+  await trpc.loadouts.getAuthBookmarked.prefetch();
 
   return {
-    props: response,
+    props: {},
   };
 };
 
